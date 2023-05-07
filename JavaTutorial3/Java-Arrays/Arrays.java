2967d5e6eb1dc20b772d1177c26a994831422434
Jorge Luis Escobedo Ocaña <jescobedooc@unsa.edu.pe>
2023-05-07 16:46:49 -0500

    Terminando de agregar los java tutorials

---
+++ JavaTutorial3/Java-Arrays/Arrays.java
@@ -0,0 +1,7 @@
+//Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+public class Main {
+  public static void main(String[] args) {
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    System.out.println(cars[0]);
+  }
+}
---
+++ JavaTutorial3/Java-ForEach/ForEach.java
@@ -0,0 +1,12 @@
+/*for (type variableName : arrayName) {
+  // code block to be executed
+}
+*/
+public class Main {
+  public static void main(String[] args) {
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    for (String i : cars) {
+      System.out.println(i);
+    }
+  }
+}
