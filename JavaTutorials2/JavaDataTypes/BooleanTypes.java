4bcec8e17869c9b09655a266b27be70b5cbcd6b8
MauCausa <121000971+MauCausa@users.noreply.github.com>
2023-05-07 16:18:42 -0500

    CommitBoolean
    1°

---
+++ JavaTutorials2/JavaDataTypes/BooleanTypes.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("A");
+
+        //Tipo de dato booleano, solo puede tomar los valores true o false
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun); // Imprime true
+        System.out.println(isFishTasty); // Imprime false
+
+    }
+}
