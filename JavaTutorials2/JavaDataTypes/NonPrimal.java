205fc680bbec282fa7987acb2c41486a5086e928
MauCausa <121000971+MauCausa@users.noreply.github.com>
2023-05-07 16:25:22 -0500

    CommitNon
    1°

---
+++ JavaTutorials2/JavaDataTypes/NonPrimal.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("A");
+
+        /*
+         * La principal diferencia entre los tipos de datos primitivos y no primitivos
+         * son:
+         * 
+         * Los tipos primitivos están predefinidos (ya definidos) en Java. Los tipos no
+         * primitivos son creados por el programador y no están definidos por Java
+         * (excepto String).
+         * Los tipos no primitivos se pueden usar para llamar a métodos para realizar
+         * ciertas operaciones, mientras que los tipos primitivos no pueden.
+         * Un tipo primitivo siempre tiene un valor, mientras que los tipos no
+         * primitivos pueden ser nulos.
+         * Un tipo primitivo comienza con una letra minúscula, mientras que los tipos no
+         * primitivos comienzan con una letra mayúscula.
+         * El tamaño de un tipo primitivo depende del tipo de datos, mientras que los
+         * tipos no primitivos tienen todos el mismo tamaño.
+         */
+    }
+}
