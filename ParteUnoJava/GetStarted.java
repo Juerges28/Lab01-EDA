fb4768ffb140fc15b761ea58fc1fccc0fb6e0471
Klismann30 <118670708+klismannSis@users.noreply.github.com>
2023-05-07 17:38:12 -0500

    Add files via upload

---
+++ ParteUnoJava/GetStarted.java
@@ -0,0 +1,10 @@
+public class GetStarted {
+    public static void main(String[] args) {
+
+        System.out.println("Intalacion de Java");
+        System.out.println("Si Java está instalado, verá algo como esto:");
+        System.out.println("java version 11.0.1 2018-10-16 LTS");
+        System.out.println("Java(TM) SE Runtime Environment 18.9 (build 11.0.1+13-LTS)");
+        System.out.println("Java HotSpot(TM) 64-Bit Server VM 18.9 (build 11.0.1+13-LTS, mixed mode)");
+    }
+}
