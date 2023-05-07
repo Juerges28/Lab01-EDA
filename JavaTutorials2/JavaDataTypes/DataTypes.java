af4930a7c45666030c38ac2cc098fb9dc5118059
MauCausa <121000971+MauCausa@users.noreply.github.com>
2023-05-07 16:06:05 -0500

    CommitDataTypes
    1°

---
+++ JavaTutorials2/JavaDataTypes/DataTypes.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("A");
+
+        int myNum = 5;               // Integer (número entero)
+        float myFloatNum = 5.99f;    // Floating (número flotante)
+        char myLetter = 'D';         // Character (caracter)
+        boolean myBool = true;       // Boolean   
+        String myText = "Hello";     // String   (cadena de texto)
+
+    }
+}
