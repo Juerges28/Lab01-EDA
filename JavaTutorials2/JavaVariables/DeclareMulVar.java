ce230a965971aaf494ff73b92a25b53177aa57a0
MauCausa <121000971+MauCausa@users.noreply.github.com>
2023-05-07 15:56:42 -0500

    CommitDeclareMulVar
    1°

---
+++ JavaTutorials2/JavaVariables/DeclareMulVar.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("A");
+
+        int x = 5;
+        int y = 6;
+        int z = 50;
+        System.out.println(x + y + z);
+
+        int x1 = 5, y1 = 6, z1 = 50;
+        System.out.println(x + y + z);
+
+        int x2, y2, z2;
+        x = y = z = 50;
+        System.out.println(x + y + z);
+
+    }
+}
