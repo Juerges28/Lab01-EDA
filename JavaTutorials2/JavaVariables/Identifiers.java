feaaf97b1dea81e727b38bba52ff2cc044f746ee
MauCausa <121000971+MauCausa@users.noreply.github.com>
2023-05-07 15:59:48 -0500

    CommitIdentifiers
    1°

---
+++ JavaTutorials2/JavaVariables/Identifiers.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("A");
+
+        // Bien
+        int minutesPerHour = 60;
+
+        // OK, pero no es sencillo entender qué representa m
+        int m = 60;
+
+    }
+}
