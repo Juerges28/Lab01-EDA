0c7d96f3b05b3f81579c66ef11637163744be90e
MauCausa <121000971+MauCausa@users.noreply.github.com>
2023-05-07 15:53:32 -0500

    CommitPrintV
    1°

---
+++ JavaTutorials2/JavaVariables/PrintVariables.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("A");
+
+        String name = "John";
+        System.out.println("Hello " + name);
+
+        String firstName = "John ";
+        String lastName = "Doe";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+
+        int x = 5;
+        int y = 6;
+        System.out.println(x + y); // Imprime el valor de x + y
+
+    }
+}
