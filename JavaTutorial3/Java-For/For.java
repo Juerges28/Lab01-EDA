dc6fe997cb6e0434fb1de0a3a7f46ac63ac3f430
Jorge Luis Escobedo Ocaña <jescobedooc@unsa.edu.pe>
2023-05-07 16:36:56 -0500

    Parte de Java Tutorial agregado

---
+++ JavaTutorial3/Java-For/For.java
@@ -0,0 +1,12 @@
+/*
+ for (statement 1; statement 2; statement 3) {
+  // code block to be executed
+}
+ */
+public class Main {
+  public static void main(String[] args) {
+    for (int i = 0; i < 5; i++) {
+      System.out.println(i);
+    }
+  }
+}
