ea72cf9ee86915a99a82d8cf3030480a5adb3b4f
Klismann30 <118670708+klismannSis@users.noreply.github.com>
2023-05-07 17:39:46 -0500

    Add files via upload

---
+++ ParteUnoJava/Sintax.java
@@ -0,0 +1,11 @@
+public class Sintax {
+    public static void main(String[] args) {
+        
+        System.out.println("Sintaxis de Java");
+        System.out.println("1. Nombre de archivo y clase en Java");
+        System.out.println("2. Convención de nombres de clase en Java");
+        System.out.println("3. El método main de Java");
+        System.out.println("4. Delimitación de bloques de código en Java");
+        System.out.println("5. El método println()");
+    }
+}
